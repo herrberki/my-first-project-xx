623b26724e6c8dbf705ea53d4bbf38430a89e5a7
berkan <brknyildiz@gmail.com>
2022-08-02 21:26:11 +0300

    add entity logs

---
+++ src/main/java/com/example/demo/UserEntity.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "User")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name= "user_surname")
+    private String userSurname;
+}
