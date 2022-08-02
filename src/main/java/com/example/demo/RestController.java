547b8248a92243a669815f66794262489ad8739c
BERKAN <berkan@BERKAN.local>
2022-08-02 15:34:59 +0300

    init

---
+++ src/main/java/com/example/demo/RestController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @PostMapping()
+    public String init(){
+        return "hello";
+    }
+
+
+}
